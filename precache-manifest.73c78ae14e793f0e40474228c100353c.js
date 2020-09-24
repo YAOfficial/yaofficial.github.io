4c68f5e11deee369bb5a7615daf81000166be1dc
CsBo1 <57628719+CsBo1@users.noreply.github.com>
2020-09-24 16:50:15 +0100

    Updates

---
+++ precache-manifest.73c78ae14e793f0e40474228c100353c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe81340cb70d0307b4b4f8303cf036c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "b86679624247dfdfdfd9",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/my-app/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b86679624247dfdfdfd9",
+    "url": "/my-app/static/js/main.fa505699.chunk.js"
+  },
+  {
+    "revision": "4f282b9659b4936d5d46",
+    "url": "/my-app/static/js/runtime-main.922ac3ad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
