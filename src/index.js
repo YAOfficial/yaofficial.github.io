fba2f10dd2b3d00c7b6918e666bc44abc80e281c
CsBo1 <57628719+CsBo1@users.noreply.github.com>
2020-09-27 15:45:57 +0100

    the first test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
-import app from "./App";
+
 import * as serviceWorker from "./serviceWorker";
 const logo = document.querySelectorAll("#logo  path");
 
