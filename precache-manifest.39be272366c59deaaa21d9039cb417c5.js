58266feef8f8fa4512f74ddb5836275e29aa75ce
CsBo1 <57628719+CsBo1@users.noreply.github.com>
2020-09-27 15:46:15 +0100

    Updates

---
+++ precache-manifest.39be272366c59deaaa21d9039cb417c5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c14e3beca6e217a7034fa4cc170b0aa0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "65ab16d47641b0a21f36",
+    "url": "/portfolio/static/css/main.df25f796.chunk.css"
+  },
+  {
+    "revision": "65ab16d47641b0a21f36",
+    "url": "/portfolio/static/js/main.9bd74ac9.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e190da137fa4742f944f49f098be30fb.js"
+  "/portfolio/precache-manifest.39be272366c59deaaa21d9039cb417c5.js"
 );
 
 self.addEventListener('message', (event) => {
