56ab3b00dcb79e1c014e48937af221867b384c6c
CsBo1 <57628719+CsBo1@users.noreply.github.com>
2020-10-14 15:43:26 +0100

    Updates

---
+++ precache-manifest.8949ea0ec352aee9c020b715f761b4e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a4d863137c6c90a3ab486b2d4d9ff6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a65db1ef06e90b6c078",
+    "url": "/static/css/main.e9927b4f.chunk.css"
+  },
+  {
+    "revision": "d5c74901fe70e0562470",
+    "url": "/static/js/2.2073934b.chunk.js"
+  },
+  {
+    "revision": "d48494fd72a624f846852e489c2c9a7e",
+    "url": "/static/js/2.2073934b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a65db1ef06e90b6c078",
+    "url": "/static/js/main.c0705482.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "ee61ca49ec2188e4ac026017e8b315b7",
+    "url": "/static/media/react-seeklogo.com.ee61ca49.svg"
+  },
+  {
+    "revision": "c4c965d2c58fa930635215917cd7a7f7",
+    "url": "/static/media/sw.c4c965d2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yaofficial-github-io/precache-manifest.025f66e5928970927cb5072d49033390.js"
+  "/precache-manifest.8949ea0ec352aee9c020b715f761b4e7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yaofficial-github-io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
